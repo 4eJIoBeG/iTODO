4678f5f19392016600381837bdf4a73eeaabb078
4eJIoBeG <pavelshashenko@gmail.com>
2024-12-24 00:57:47 +0300

    init auth resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common"
 import { TasksModule } from "./tasks/tasks.module"
 import { PrismaModule } from "prisma/prisma.module"
+import { UsersModule } from "./users/users.module"
+import { AuthModule } from "./auth/auth.module"
 
 @Module({
-	imports: [PrismaModule, TasksModule]
+	imports: [PrismaModule, TasksModule, UsersModule, AuthModule]
 })
 export class AppModule {}
